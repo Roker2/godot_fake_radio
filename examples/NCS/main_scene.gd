f1456aac2a4789f95d5a06edda9a3ed7ce326cc0
Roker2 <dimaminko2000@gmail.com>
2024-08-22 20:58:07 +0200

    examples/NCS/main_scene: Improve example and update it

--- examples/NCS/main_scene.gd
+++ examples/NCS/main_scene.gd
@@ -5,5 +5,10 @@ extends Node2D
 func _ready() -> void:
 	radio.play()
 
-func _on_button_pressed() -> void:
-	radio.switch_to(1)
+
+func _on_button_next_pressed() -> void:
+	radio.switch_to_next()
+
+
+func _on_button_prev_pressed() -> void:
+	radio.switch_to_prev()
