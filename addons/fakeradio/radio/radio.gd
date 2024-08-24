091bb7cc6c8e9db9b67a72043d84e89521d6661a
Roker2 <dimaminko2000@gmail.com>
2024-08-24 18:11:44 +0200

    radio: Hide some variables

--- addons/fakeradio/radio/radio.gd
+++ addons/fakeradio/radio/radio.gd
@@ -6,47 +6,47 @@ signal station_is_changed
 
 @export var current_station = 0
 @export_enum("Idle", "Physics") var process_callback: String = "Idle"
-var stations: Array[Station]
-var stopwatch: float = 0
-var process_stopwatch_in_physics: bool = false
+var _stations: Array[Station]
+var _stopwatch: float = 0
+var _process_stopwatch_in_physics: bool = false
 
 func _ready() -> void:
 	for child in get_children():
 		if child is Station:
-			stations.append(child)
+			_stations.append(child)
 	stream = _get_current_station().get_station_playlist()
 	
 	if process_callback == "Physics":
-		process_stopwatch_in_physics = true
+		_process_stopwatch_in_physics = true
 
 func play_radio() -> void:
+	play(_stopwatch)
 	station_is_changed.emit()
-	play(stopwatch)
 
 func stop_radio() -> void:
-	station_is_changed.emit()
 	stop()
+	station_is_changed.emit()
 
 func switch_to(index: int) -> void:
-	if index >= stations.size():
+	if index >= _stations.size():
 		printerr("index is not valid")
 		return
 	var prev_playing = playing
-	stream = stations[index].get_station_playlist()
+	stream = _stations[index].get_station_playlist()
 	current_station = index
 	if prev_playing:
-		play(stopwatch)
+		play(_stopwatch)
 	station_is_changed.emit()
 
 func switch_to_next() -> void:
-	if current_station == stations.size() - 1:
+	if current_station == _stations.size() - 1:
 		switch_to(0)
 	else:
 		switch_to(current_station + 1)
 
 func switch_to_prev() -> void:
 	if current_station == 0:
-		switch_to(stations.size() - 1)
+		switch_to(_stations.size() - 1)
 	else:
 		switch_to(current_station - 1)
 
@@ -57,12 +57,12 @@ func get_station_description() -> String:
 	return _get_current_station().station_description
 
 func _get_current_station() -> Station:
-	return stations[current_station]
+	return _stations[current_station]
 
 func _process(delta: float) -> void:
-	if not process_stopwatch_in_physics:
-		stopwatch += delta
+	if not _process_stopwatch_in_physics:
+		_stopwatch += delta
 
 func _physics_process(delta: float) -> void:
-	if process_stopwatch_in_physics:
-		stopwatch += delta
+	if _process_stopwatch_in_physics:
+		_stopwatch += delta
