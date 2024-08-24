6a11174bf6fef0ae198b243587dbac3d3f0429ed
Roker2 <dimaminko2000@gmail.com>
2024-08-24 14:34:07 +0200

    radio: Implement stopwatch

--- addons/fakeradio/radio/radio.gd
+++ addons/fakeradio/radio/radio.gd
@@ -3,7 +3,10 @@ extends AudioStreamPlayer
 class_name Radio
 
 @export var current_station = 0
+@export_enum("Idle", "Physics") var process_callback: String = "Idle"
 var stations: Array[Station]
+var stopwatch: float = 0
+var process_stopwatch_in_physics: bool = false
 
 func _ready() -> void:
 	for child in get_children():
@@ -16,11 +19,10 @@ func switch_to(index: int) -> void:
 		printerr("index is not valid")
 		return
 	var prev_playing = playing
-	var position = get_playback_position()
 	stream = stations[index].get_station_playlist()
 	current_station = index
 	if prev_playing:
-		play(position)
+		play(stopwatch)
 
 func switch_to_next() -> void:
 	if current_station == stations.size() - 1:
@@ -33,3 +35,11 @@ func switch_to_prev() -> void:
 		switch_to(stations.size() - 1)
 	else:
 		switch_to(current_station - 1)
+
+func _process(delta: float) -> void:
+	if not process_stopwatch_in_physics:
+		stopwatch += delta
+
+func _physics_process(delta: float) -> void:
+	if process_stopwatch_in_physics:
+		stopwatch += delta
