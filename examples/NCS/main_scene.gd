08ecf8031c959507e4ee6748ff9fa75462309558
Roker2 <dimaminko2000@gmail.com>
2024-08-24 18:12:04 +0200

    examples/NCS/main_scene: Update

--- examples/NCS/main_scene.gd
+++ examples/NCS/main_scene.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
 @onready var radio = $Radio
+@onready var label_name = $CanvasLayer/VBoxContainer/LabelRadioName
+@onready var label_desc = $CanvasLayer/VBoxContainer/LabelRadioDesc
 
 func _ready() -> void:
-	radio.play()
+	radio.station_is_changed.connect(update_info)
+	radio.play_radio()
+
+
+func update_info() -> void:
+	label_name.text = radio.get_station_name()
+	label_desc.text = radio.get_station_description()
 
 
 func _on_button_next_pressed() -> void:
